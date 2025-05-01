0ed3414e293f0693594d49bbdeb2be240b1d9e86
k <handykameron@live.com>
2025-05-01 14:48:51 -0800

    removed the Api Key

---
+++ javascript.js
@@ -0,0 +1,3 @@
+async function  GetGiffy() {
+    let GetUrl = fetch("https://api.giphy.com/v1/gifs/search?api_key=PCPnD7VRc3Rrth8CRIFNef5OF2INPY6v&q=nature&limit=10&offset=0&rating=g&lang=en&bundle=messaging_non_clips")
+}
