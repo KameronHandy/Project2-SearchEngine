615b2fd342462047c3d0f3fb8daba30b0a8dd52a
k <handykameron@live.com>
2025-05-01 18:52:35 -0800

    searchbar is working, now moving on to the styling of webpage

--- javascript.js
+++ javascript.js
@@ -1,21 +1,23 @@
 async function  GetGiffy() {
     let Search = document.getElementById("Search").value
-    let GetUrl = fetch(`https://api.giphy.com/v1/gifs/search?api_key=${Key}v&q=${Search}&limit=10&offset=0&rating=g&lang=en&bundle=messaging_non_clips`)
-    let SearchResponseHolder = respone.json()
-    let SearchDataHolder= SearchResponseHolder.data()
+    let GetUrl = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=${Key}v&q=${Search}&limit=10&offset=0&rating=g&lang=en&bundle=messaging_non_clips`)
+    let SearchResponseHolder = await GetUrl.json();
+    let SearchDataHolder= await SearchResponseHolder.data;
 
-    let ImageHolder= document.getElementById(Images);
-    ImageHolder.innerHTML
+    let ImageHolder= document.getElementById("Images");
+    ImageHolder.innerHTML = "";
 
 
-    for (let index = 0; index < ImageHolder.length; index++)
-        image = document.CreateElement("img");
-        image.src=SearchResponseHolder[index].images.original.url;
-        container.append;
-        console.log(image.src=SearchResponseHolder[index].images.original.url);
-
+    for (let index = 0; index < SearchDataHolder.length; index++){
+        let image = document.createElement("img");
+        image.src=SearchDataHolder[index].images.original.url;
+        ImageHolder.append(image);
+        console.log(SearchDataHolder[index].images.original.url);
+    }
 }
 
+GetGiffy();
 
 
+document.getElementById("GiffButton").addEventListener("click",GetGiffy)
 
