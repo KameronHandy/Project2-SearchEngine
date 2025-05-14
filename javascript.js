576714b26124dcdb72c1b3f299fb73ae7cd001f6
k <handykameron@live.com>
2025-05-14 14:08:19 -0800

    Successfully attached JavaScript to Css class to make uniform sized gifs

--- javascript.js
+++ javascript.js
@@ -11,57 +11,53 @@ async function  GetGiffy() {
 
     for (let index = 0; index < SearchDataHolder.length; index++){
         let image = document.createElement("img");
-        image.src=SearchDataHolder[index].images.original.url;
-        ImageHolder.append(image);
-        console.log(SearchDataHolder[index].images.original.url);
-
-        
-
-        
+        image.className="imgAnimatedGif"
+        image.src = SearchDataHolder[index].images.original.url
+        ImageHolder.append(image);  
     }
 }
 
 
 
-async function  GetGiffyRandom() {
-    let GetUrl = await fetch(`https://api.giphy.com/v1/gifs/random?api_key=${KeyRandom}&tag=&rating=g`)
-    let RandomResponseHolder = await GetUrl.json();
-    let RandomDataHolder= await RandomResponseHolder.data;
+// async function  GetGiffyRandom() {
+//     let GetUrl = await fetch(`https://api.giphy.com/v1/gifs/random?api_key=${KeyRandom}&tag=&rating=g`)
+//     let RandomResponseHolder = await GetUrl.json();
+//     let RandomDataHolder= await RandomResponseHolder.data;
 
-    let RandomImageHolder= document.getElementById("RandomImages");
+// //    let RandomImageHolder= document.getElementById("RandomImages");
 
-    for (let index = 0; index < RandomDataHolder.length; index++){
-        let newElement = document.createElement("img");
-        newElement.src = RandomDataHolder [index].images.original.url;
-        newElement.ClassName = "profileImage";
-        document.getElementById("RandomImages").append(newElement);
-        console.log(RandomDataHolder[index].images.original.url);
-    }
+//     for (let index = 0; index < RandomDataHolder.length; index++){
+//         let newElement = document.createElement("img");
+//         newElement.src = RandomDataHolder [index].images.original.url;
+//         newElement.ClassName = "profileImage";
+//         document.getElementById("RandomImages").append(newElement);
+//         console.log(RandomDataHolder[index].images.original.url);
+//     }
 
-}
+// }
 
 
-function addElements(){
-    let newElement = document.createAttribute("img")
-    newElement.src = RandomDataHolder [index].images.original.url;
-    newElement.ClassName = "profileImage"
-    document.getElementById("section_image").append(newElement)
-    console.log(RandomDataHolder[index].images.original.url);
-}
+// function addElements(){
+//     let newElement = document.createAttribute("img")
+//     newElement.src = RandomDataHolder [index].images.original.url;
+//     newElement.ClassName = "profileImage"
+//     document.getElementById("section_image").append(newElement)
+//     console.log(RandomDataHolder[index].images.original.url);
+// }
 
-function deleteElements () {
-    let arrElements = document.getElementsByTagName("img")
-    for (let index=0; index < arrElements.length; index++){
-        arrElements[index].remove()
-    }
+// function deleteElements () {
+//     let arrElements = document.getElementsByTagName("img")
+//     for (let index=0; index < arrElements.length; index++){
+//         arrElements[index].remove()
+//     }
 
-}
+// }
 
 
 GetGiffy();
-GetGiffyRandom();
+//GetGiffyRandom();
 
 
 document.getElementById("GiffButton").addEventListener("click",GetGiffy)
-document.getElementById("Random").addEventListener("click",GetGiffyRandom)
+//document.getElementById("Random").addEventListener("click",GetGiffyRandom)
 
